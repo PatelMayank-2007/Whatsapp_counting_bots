774332f3044df5cd26b72a997c0c0cefbf5ee8d9
Mayank Patel <patelmayank232007@gmail.com>
2026-05-23 14:13:28 +0530

    render deployment

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Bot is running");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server started");
+});
+
+
+
+
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const fs = require('fs');
